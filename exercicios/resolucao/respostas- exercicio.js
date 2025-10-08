d2c9ec92ec629bcef5deca86b20092b91aaac4c5
RPV <rpv@senai.br>
2025-10-07 22:19:15 -0300

    Aula dia 07

---
+++ exercicios/resolucao/respostas- exercicio.js
@@ -0,0 +1,32 @@
+//1. Se ontem foi quarta-feira, amanhã será...
+//qual é o contexto da pergunta? fala sobre o que?
+// sabendo o "oque está sendo falado!", existe regras?
+// dias da semana
+// máximo de 7 dias
+//
+// let ontem = 'quarta-feira'
+// let hoje = 'quinta-feira'
+// let amanhã = 'sexta-feira'
+console.log('1. Se ontem foi quarta-feira, amanhã será...')
+console.log('R: sexta-feira')
+console.log('### -------------------------------- ###')
+
+//2. Complete a sequência: 1, 4, 9, 16, 25, ...
+console.log('2. Complete a sequência: 1, 4, 9, 16, 25, ...')
+console.log('R: Sequencia: 1, 4, 9, 16, 25, 36, 49...')
+console.log('### --------------------------------- ###')
+
+//3. Um trem de 100 metros de comprimento viaja a 50 km/h. Quanto tempo levará para atravessar um túnel de 50 metros?
+console.log('3. Um trem de 100 metros de comprimento viaja a 50 km/h. Quanto tempo levará para atravessar um túnel de 50 metros?')
+let conta
+let metros = 150
+let velocidade = 50
+let velocidade_metros = (velocidade * 1000) / 3600
+conta = metros / velocidade_metros
+console.log('R: Ele levará '+ conta.toFixed(2) + ' segundos para atravessar o tunel. (50 metros do tunel + 100 metros do trem)')
+console.log('### --------------------------------- ###')
+
+//4. Júlia é mais alta que Ana. Ana é mais baixa que Bruno. Qual dos três é o mais alto?
+console.log('4. Júlia é mais alta que Ana. Ana é mais baixa que Bruno. Qual dos três é o mais alto?')
+console.log('R: Júlia é a mais alta dos três. Mais alta que Ana (mais letras e mais a frente no alfabeto) e mais alta que Bruno (Mesma quantidade de letras, porém contém caracter especial e está mais a frende do no alfabeto)')
+console.log('### --------------------------------- ###')
